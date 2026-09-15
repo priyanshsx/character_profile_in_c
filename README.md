e3bf8c4cee178448ee3d4d0c46e21845b2caf289
Priyansh <saxenapriyansh95@gmail.com>
2026-09-15 09:31:37 +0530

    added the structure for the program

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Character Profiles in C 
 
+This program in C does the following: 
+- defines a custom Player data type, 
+- creates a character,  
+- updates their stats, and 
+- prints their profile to he terminal 
--- character_profile.c
+++ character_profile.c
@@ -2,4 +2,15 @@
 
 #include<stdio.h>
 #include<stdlib.h> 
+#include<string.h>
 
+// define a customer player data type 
+
+
+// create a character 
+
+
+// update their status 
+
+
+// print their profile to the terminal 
